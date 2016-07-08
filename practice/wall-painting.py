1d94cf64c29a87660bbc494a5af25dadd2a908e0
Stephen <stevemuller@gmail.com>
2016-07-08 14:19:20 -0700

    fixed final calculation

--- practice/wall-painting.py
+++ practice/wall-painting.py
@@ -2,41 +2,44 @@
 # pull in math for cieling function#
 import math
 # set wall area to 0#
-wallarea = 0
+wall_area = 0
+# 1 gallon covers 400sq ft
+ONE_GALLON_AREA = 400 # square feet
 
+# establish the user data
 print('How many walls do you plan on painting?')
-wallstopaint = int(input())
-
+walls_to_paint = int(input())
 print('how many coats of paint do you plan on doing?')
 coats = int(input())
-
 print('How much does paint cost per gallon?')
-paintcost = float(input())
+paint_cost = float(input())
 
-while wallstopaint > 0:
+while walls_to_paint > 0:
     print('How wide is your wall?')
-    wallwidth = float(input())
-
+    wall_width = float(input())
     print('How high is your wall?')
-    wallheight = float(input())
-    wallarea = wallwidth * wallheight + wallarea
-    wallstopaint = wallstopaint - 1
-    if wallstopaint >= 1:
+    wall_height = float(input())
+    wall_area = wall_width * wall_height + wall_area
+    walls_to_paint = walls_to_paint - 1
+    if walls_to_paint >= 1:
         print('and the next wall:')
         pass
     pass
 
-print('wallarea = ' + str(int(wallarea)) + ' sq ft')
+print('wallarea = ' + str(int(wall_area)) + ' sq ft')
 
 # 400 is the the sq ft value one gallon covers
-gallonsneeded = wallarea / 400
+gallons_needed = wall_area / ONE_GALLON_AREA
 print('the number of coats is ' + str(int(coats)))
-print('gallonsneeded = ' + str(float(gallonsneeded)))
+print('gallonsneeded = ' + str(float(gallons_needed)))
 
-math.ceil(gallonsneeded)
-gallonsneeded = math.ceil(gallonsneeded) * coats
-print('gallonsneeded = ' + str(float(gallonsneeded)))
+gallons_needed = gallons_needed * coats
+gallons_needed = math.ceil(gallons_needed)
+print('gallonsneeded = ' + str(float(gallons_needed)))
 
 
-totalcost = gallonsneeded * paintcost
-print('your project will cost $' + str(int(totalcost)))
+total_cost = gallons_needed * paint_cost
+print('your project will cost $' + str(int(total_cost)))
+
+
+# it's calculating the amount of paint vs
