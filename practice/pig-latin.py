2eeab3ef7ab72dd869a948d03163ed52aac23e01
Stephen <stevemuller@gmail.com>
2016-07-11 16:26:50 -0700

    added the caps save function

--- practice/pig-latin.py
+++ practice/pig-latin.py
@@ -1,4 +1,9 @@
 """turn words into pig latin"""
+####preserve caps eg Cat = Atcay
+####preserve punctuation eg boy. = oybay.
+####handle >3 consonants elegantly
+
+
 # setup
 
 VOWEL = 'aeiou'
@@ -8,7 +13,13 @@ word = input('word to translate: ')
 if word == '':
     word = 'apple'
 
-# transform
+#transform
+
+if word[0].isupper():
+    capital = True
+else:
+    capital = False
+
 
 # fix case issues
 no_caps_word = str.lower(word)
@@ -28,6 +39,9 @@ else:
     else:
         pig_latin_suffix = first_letter + 'ay'
         pig_latin_word = no_caps_word[1:] + pig_latin_suffix
+if capital == True:
+    print('finally!')
+    pig_latin_word = pig_latin_word.capitalize()
 
 # output
 print('Your word in pig latin is: ' + pig_latin_word)
