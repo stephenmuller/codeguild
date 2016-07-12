8ea7a98816fa3daee3e9fcc7e4b0c5f94f0f405e
Stephen <stevemuller@gmail.com>
2016-07-11 20:04:46 -0700

    some progress

--- practice/spelling.py
+++ practice/spelling.py
@@ -1,9 +1,24 @@
 """check if a word follows the I before E rule"""
 
 # setup
+ie_check = 'ie'
+ei_check = 'ei'
+cie_check = 'cie'
+# input
 
-#input
+word = input('word to check: ')
+if word == '':
+    word = 'ceiling'
 
 # transform
+
+if ie_check in word:
+    if cie_check in word:
+        print(word + ' does follow the rule')
+    print('wohoo')
+
+
+else:
+    print('ugh.')
 
 # output
